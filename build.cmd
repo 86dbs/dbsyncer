00a103ca4d3ff982eceba811613fbab959479fb3
AE86 <836391306@qq.com>
2021-02-02 18:01:29 +0800

    build cmd

---
+++ build.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+echo "Clean Project ..."
+call mvn clean -f pom.xml
+
+echo "Build Project ..."
+call mvn compile package -f pom.xml -D"maven.test.skip=true"
+
+set CP_PATH=%~dp0
+xcopy %CP_PATH%dbsyncer-web\target\dbsyncer-*.zip %CP_PATH% /y
+
+:exit
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mvn clean -f pom.xml
+mvn compile package -f pom.xml -Dmaven.test.skip=true
+
+CURRENT_DIR=$(pwd);
+cp -rvf $CURRENT_DIR/dbsyncer-web/target/dbsyncer-*.zip $CURRENT_DIR
