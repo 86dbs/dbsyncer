cc832eafb658862c20ea885a6355bc067bd0172b
AE86 <836391306@qq.com>
2021-02-02 18:25:29 +0800

    循环引用

--- dbsyncer-common/src/main/java/org/dbsyncer/common/util/JsonUtil.java
+++ dbsyncer-common/src/main/java/org/dbsyncer/common/util/JsonUtil.java
@@ -1,11 +1,12 @@
 package org.dbsyncer.common.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 public abstract class JsonUtil {
 
     public static String objToJson(Object obj) {
-        return JSON.toJSONString(obj);
+        return JSON.toJSONString(obj, SerializerFeature.DisableCircularReferenceDetect);
     }
 
     public static <T> T jsonToObj(String json, Class<T> valueType) {
