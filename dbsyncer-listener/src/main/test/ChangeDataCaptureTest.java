22a40b7a44551b0bf69577a72208bc9ff68cfceb
AE86 <836391306@qq.com>
2021-06-14 01:54:09 +0800

    fix test

--- dbsyncer-listener/src/main/test/ChangeDataCaptureTest.java
+++ dbsyncer-listener/src/main/test/ChangeDataCaptureTest.java
@@ -6,46 +6,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @version 1.0.0
  * @Author AE86
- * @Date 2021-05-18 22:25
+ * @Date 2021-06-14 01:55
  * @see https://www.red-gate.com/simple-talk/sql/learn-sql-server/introduction-to-change-data-capture-cdc-in-sql-server-2008/
  */
 public class ChangeDataCaptureTest {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private static final String GET_AGENT_SERVER_STATE = "EXEC master.dbo.xp_servicecontrol N'QUERYSTATE', N'SQLSERVERAGENT'";
-    private static final String GET_DATABASE_NAME = "SELECT db_name()";
-    private static final String IS_CDC_ENABLED = "SELECT is_cdc_enabled FROM sys.databases WHERE name = '#'";
-
-    private static final String GET_MAX_TRANSACTION_LSN = "SELECT MAX(start_lsn) FROM cdc.lsn_time_mapping WHERE tran_id <> 0x00";
-    private static final String GET_MIN_LSN = "SELECT sys.fn_cdc_get_min_lsn('#')";
-    private static final String GET_MAX_LSN = "SELECT sys.fn_cdc_get_max_lsn()";
-
-    private static final String LOCK_TABLE = "SELECT * FROM [#] WITH (TABLOCKX)";
-    private static final String GET_ALL_CHANGES_FOR_TABLE = "SELECT *# FROM cdc.[fn_cdc_get_all_changes_#](?, ?, N'all update old') order by [__$start_lsn] ASC, [__$seqval] ASC, [__$operation] ASC";
-    private static final String GET_LIST_OF_CDC_ENABLED_TABLES = "EXEC sys.sp_cdc_help_change_data_capture";
-    private static final String SQL_SERVER_VERSION = "SELECT @@VERSION AS 'SQL Server Version'";
-    private static final String LSN_TIMESTAMP_SELECT_STATEMENT = "sys.fn_cdc_map_lsn_to_time([__$start_lsn])";
-    private static final String AT_TIME_ZONE_UTC = "AT TIME ZONE 'UTC'";
-
     private static final String STATEMENTS_PLACEHOLDER = "#";
-    private static final Pattern BRACKET_PATTERN = Pattern.compile("[\\[\\]]");
+    private static final String GET_DATABASE_NAME = "SELECT db_name()";
+    private static final String GET_DATABASE_VERSION = "SELECT @@VERSION AS 'SQL Server Version'";
+    private static final String IS_SERVER_AGENT_RUNNING = "EXEC master.dbo.xp_servicecontrol N'QUERYSTATE', N'SQLSERVERAGENT'";
+    private static final String IS_DB_CDC_ENABLED = "SELECT is_cdc_enabled FROM sys.databases WHERE name = '#'";
+    private static final String IS_TABLE_CDC_ENABLED = "SELECT COUNT(*) FROM sys.tables tb WHERE tb.is_tracked_by_cdc = 1 AND tb.name='#'";
+    private static final String ENABLE_DB_CDC = "IF EXISTS(select 1 from sys.databases where name = '#' AND is_cdc_enabled=0) EXEC sys.sp_cdc_enable_db";
+    private static final String ENABLE_TABLE_CDC = "IF EXISTS(select 1 from sys.tables where name = '#' AND is_tracked_by_cdc=0) EXEC sys.sp_cdc_enable_table @source_schema = N'dbo', @source_name = N'#', @role_name = NULL, @supports_net_changes = 0";
 
-    /**
-     * 数据库的实际名称，可能与连接器配置中给定的数据库名称不同
-     */
+    private static final String AT_TIME_ZONE_UTC = " AT TIME ZONE 'UTC'";
+    private static final String GET_ALL_CHANGES_FOR_TABLE = "SELECT sys.fn_cdc_map_lsn_to_time([__$start_lsn])#, *  FROM cdc.[fn_cdc_get_all_changes_#](?, ?, N'all update old') order by [__$start_lsn] ASC, [__$seqval] ASC, [__$operation] ASC";
+    private static final String GET_TABLES_CDC_ENABLED = "EXEC sys.sp_cdc_help_change_data_capture";
+    private static final String GET_MAX_TRANSACTION_LSN = "SELECT MAX(start_lsn) FROM cdc.lsn_time_mapping WHERE tran_id <> 0x00";
+    private static final String GET_MAX_LSN = "SELECT sys.fn_cdc_get_max_lsn()";
+    private static final String GET_MIN_LSN = "SELECT sys.fn_cdc_get_min_lsn('#')";
+
     private String realDatabaseName;
     private String getAllChangesForTable;
-    private String agentState;
-    private boolean enabledCDC;
-
     private Connection connection = null;
 
     /**
@@ -54,7 +49,7 @@ public class ChangeDataCaptureTest {
      * <p>cdc.ddl_history – 此表包含自启用捕获数据以来所有 DDL 更改的历史记录。</p>
      * <p>cdc.index_columns – 该表包含与变更表相关的索引。</p>
      * <p>cdc.lsn_time_mapping – 此表映射 LSN编号(唯一序列号标识, 增加数字) 和时间。</p>
-     * <p>cdc.fn_cdc_get_all_changes_HumanResources_Shift - 可用于获取在特定时间段内发生的事件</p>
+     * <p>cdc.fn_cdc_get_all_changes_MY_USER - 可用于获取在特定时间段内发生的事件</p>
      * <p>sys.fn_cdc_map_time_to_lsn - 表中是否有 tran_end_time值大于或等于指定时间的行。例如，可以用此查询来确定捕获进程是否已处理完截至前指定时间提交的更改</p>
      * <p>sys.fn_cdc_get_max_lsn</p>
      * <p>sys.sp_cdc_cleanup_change_table 默认情况下间隔为3天清理日志数据</p>
@@ -62,38 +57,55 @@ public class ChangeDataCaptureTest {
      * @throws SQLException
      */
     @Test
-    public void testConnect() throws SQLException {
+    public void testConnect() throws SQLException, InterruptedException {
         ChangeDataCaptureTest cdc = new ChangeDataCaptureTest();
         cdc.start();
 
         // 获取数据库名 test
         realDatabaseName = cdc.queryAndMap(GET_DATABASE_NAME, rs -> rs.getString(1));
         logger.info("数据库名:{}", realDatabaseName);
-
-        boolean supportsAtTimeZone = supportsAtTimeZone(cdc);
-        logger.info("支持时区:{}", supportsAtTimeZone);
-        getAllChangesForTable = GET_ALL_CHANGES_FOR_TABLE.replaceFirst(STATEMENTS_PLACEHOLDER, Matcher.quoteReplacement(lsnTimestampSelectStatement(supportsAtTimeZone)));
-
+        // As per https://www.mssqltips.com/sqlservertip/1140/how-to-tell-what-sql-server-version-you-are-running/
+        // Always beginning with 'Microsoft SQL Server NNNN' but only in case SQL Server is standalone
+        String version = cdc.queryAndMap(GET_DATABASE_VERSION, rs -> rs.getString(1));
+        boolean supportsAtTimeZone = false;
+        if (version.startsWith("Microsoft SQL Server ")) {
+            supportsAtTimeZone = 2016 < Integer.valueOf(version.substring(21, 25));
+        }
+        logger.info("数据库版本:{}", version);
         // 获取Agent服务状态 Stopped. Running.
-        agentState = cdc.queryAndMap(GET_AGENT_SERVER_STATE, rs -> rs.getString(1));
-        logger.info("Agent服务状态:{}", agentState);
+        boolean enabledServerAgent = cdc.queryAndMap(IS_SERVER_AGENT_RUNNING, rs -> "Running.".equals(rs.getString(1)));
+        logger.info("是否启动Agent服务:{}", enabledServerAgent);
         // 获取数据库CDC状态 false 0 true 1
-        enabledCDC = cdc.queryAndMap(IS_CDC_ENABLED.replace(STATEMENTS_PLACEHOLDER, realDatabaseName), rs -> rs.getBoolean(1));
-        logger.info("CDC状态:{}", enabledCDC);
+        boolean enabledCDC = cdc.queryAndMap(IS_DB_CDC_ENABLED.replace(STATEMENTS_PLACEHOLDER, realDatabaseName), rs -> rs.getBoolean(1));
+        logger.info("是否启用库CDC:{}", enabledCDC);
+        if (!enabledCDC) {
+            cdc.execute(ENABLE_DB_CDC.replace(STATEMENTS_PLACEHOLDER, realDatabaseName));
+            // make sure DB has cdc-enabled before proceeding
+            TimeUnit.SECONDS.sleep(5);
+        }
+        boolean enabledTableCDC = cdc.queryAndMap(IS_TABLE_CDC_ENABLED.replace(STATEMENTS_PLACEHOLDER, "MY_USER"), rs -> rs.getInt(1) > 0);
+        logger.info("是否启用表CDC:{}", enabledTableCDC);
+        if (!enabledTableCDC) {
+            cdc.execute(ENABLE_TABLE_CDC.replace(STATEMENTS_PLACEHOLDER, "MY_USER"));
+        }
+        Lsn minLsn = cdc.queryAndMap(GET_MIN_LSN.replace(STATEMENTS_PLACEHOLDER, "MY_USER"), rs -> new Lsn(rs.getBytes(1)));
+        logger.info("表最早记录LSN:{}", minLsn);
 
-        // 从只读复制副本读取时，始终启用默认和唯一事务隔离是快照。这意味着CDC元数据对于长时间运行的事务不可见。
-        //因此，有必要在每次读取之前重新启动事务。对于R/W数据库，执行常规提交以保持TempDB的大小是很重要的
-//        connection.commit();
+        // 支持UTC
+        if (supportsAtTimeZone) {
+            getAllChangesForTable = GET_ALL_CHANGES_FOR_TABLE.replaceFirst(STATEMENTS_PLACEHOLDER, Matcher.quoteReplacement(AT_TIME_ZONE_UTC));
+        } else {
+            getAllChangesForTable = GET_ALL_CHANGES_FOR_TABLE;
+        }
 
         // 读取LSN 00000017:0000080d:0008
         Lsn maxTransactionLsn = cdc.queryAndMap(GET_MAX_TRANSACTION_LSN, rs -> new Lsn(rs.getBytes(1)));
         logger.info("最新事务LSN:{}", maxTransactionLsn);
-
-        final Lsn maxLsn = cdc.queryAndMap(GET_MAX_LSN, rs -> new Lsn(rs.getBytes(1)));
+        Lsn maxLsn = cdc.queryAndMap(GET_MAX_LSN, rs -> new Lsn(rs.getBytes(1)));
         logger.info("最新记录LSN:{}", maxLsn);
 
         // 读取增量
-        Set<SqlServerChangeTable> changeTables = cdc.queryAndMapList(GET_LIST_OF_CDC_ENABLED_TABLES, rs -> {
+        Set<SqlServerChangeTable> changeTables = cdc.queryAndMapList(GET_TABLES_CDC_ENABLED, rs -> {
             final Set<SqlServerChangeTable> tables = new HashSet<>();
             while (rs.next()) {
                 SqlServerChangeTable changeTable = new SqlServerChangeTable(
@@ -122,8 +134,7 @@ public class ChangeDataCaptureTest {
             changeTables.forEach(changeTable -> {
                 try {
                     final String query = getAllChangesForTable.replace(STATEMENTS_PLACEHOLDER, changeTable.getCaptureInstance());
-                    Lsn startLsn = cdc.queryAndMap(GET_MIN_LSN, rs -> new Lsn(rs.getBytes(1)));
-                    changeTable.setStartLsn(maxTransactionLsn.compareTo(startLsn) > 0 ? maxTransactionLsn.getBinary() : startLsn.getBinary());
+//                    changeTable.setStartLsn(maxTransactionLsn.getBinary());
                     changeTable.setStopLsn(maxLsn.getBinary());
                     logger.info("Getting changes for table {} in range[{}, {}]", changeTable.getTableName(), changeTable.getStartLsn(), changeTable.getStopLsn());
 
@@ -149,10 +160,24 @@ public class ChangeDataCaptureTest {
                 }
             });
         }
-        // Terminate the transaction otherwise CDC could not be disabled for tables
-//        connection.rollback();
 
         cdc.close();
+    }
+
+    private void execute(String... sqlStatements) throws SQLException {
+        Statement statement = connection.createStatement();
+        try {
+            for (String sqlStatement : sqlStatements) {
+                if (sqlStatement != null) {
+                    logger.info("executing '{}'", sqlStatement);
+                    statement.execute(sqlStatement);
+                }
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        } finally {
+            close(statement);
+        }
     }
 
     private void start() throws SQLException {
@@ -243,41 +268,6 @@ public class ChangeDataCaptureTest {
                 logger.error(e.getMessage());
             }
         }
-    }
-
-    /**
-     * Returns the query for obtaining the LSN-to-TIMESTAMP query. On SQL Server
-     * 2016 and newer, the query will normalize the value to UTC. This means that
-     * the SERVER_TIMEZONE is not necessary to be given. The returned TIMESTAMP will
-     * be adjusted by the JDBC driver using this VM's TZ (as required by the JDBC
-     * spec), and that same TZ will be applied when converting
-     * the TIMESTAMP value into an {@code Instant}.
-     */
-    private String lsnTimestampSelectStatement(boolean supportsAtTimeZone) {
-        String result = ", " + LSN_TIMESTAMP_SELECT_STATEMENT;
-        if (supportsAtTimeZone) {
-            result += " " + AT_TIME_ZONE_UTC;
-        }
-        return result;
-    }
-
-    /**
-     * 2016版本以上支持时区
-     */
-    private boolean supportsAtTimeZone(ChangeDataCaptureTest cdc) {
-        try {
-            // As per https://www.mssqltips.com/sqlservertip/1140/how-to-tell-what-sql-server-version-you-are-running/
-            // Always beginning with 'Microsoft SQL Server NNNN' but only in case SQL Server is standalone
-            logger.info("query version");
-            String version = cdc.queryAndMap(SQL_SERVER_VERSION, rs -> rs.getString(1));
-            if (version.startsWith("Microsoft SQL Server ")) {
-                Integer num = Integer.valueOf(version.substring(21, 25));
-                return num > 2016;
-            }
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
-        return false;
     }
 
 }
