80156267b0fe1798eadacd82d3e5bf70f8701d9d
AE86 <836391306@qq.com>
2021-06-19 02:07:03 +0800

    fix bug

--- dbsyncer-listener/src/main/java/org/dbsyncer/listener/sqlserver/SqlServerExtractor.java
+++ dbsyncer-listener/src/main/java/org/dbsyncer/listener/sqlserver/SqlServerExtractor.java
@@ -3,8 +3,10 @@ package org.dbsyncer.listener.sqlserver;
 import com.microsoft.sqlserver.jdbc.SQLServerConnection;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import org.apache.commons.lang.math.RandomUtils;
+import org.dbsyncer.common.event.RowChangedEvent;
 import org.dbsyncer.common.util.CollectionUtils;
 import org.dbsyncer.connector.config.DatabaseConfig;
+import org.dbsyncer.connector.constant.ConnectorConstant;
 import org.dbsyncer.listener.AbstractExtractor;
 import org.dbsyncer.listener.ListenerException;
 import org.slf4j.Logger;
@@ -18,7 +20,6 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.regex.Matcher;
 
 /**
  * @version 1.0.0
@@ -39,18 +40,17 @@ public class SqlServerExtractor extends AbstractExtractor {
     private static final String ENABLE_DB_CDC = "IF EXISTS(select 1 from sys.databases where name = '#' AND is_cdc_enabled=0) EXEC sys.sp_cdc_enable_db";
     private static final String ENABLE_TABLE_CDC = "IF EXISTS(select 1 from sys.tables where name = '#' AND is_tracked_by_cdc=0) EXEC sys.sp_cdc_enable_table @source_schema = N'dbo', @source_name = N'#', @role_name = NULL, @supports_net_changes = 0";
     private static final String DISABLE_TABLE_CDC = "EXEC sys.sp_cdc_disable_table @source_schema = N'dbo', @source_name = N'#', @capture_instance = 'all'";
-
-    private static final String AT_TIME_ZONE_UTC = " AT TIME ZONE 'UTC'";
-    private static final String GET_ALL_CHANGES_FOR_TABLE = "SELECT sys.fn_cdc_map_lsn_to_time([__$start_lsn])#, * FROM cdc.[fn_cdc_get_all_changes_#](?, ?, N'all update old') order by [__$start_lsn] ASC, [__$seqval] ASC, [__$operation] ASC";
     private static final String GET_TABLES_CDC_ENABLED = "EXEC sys.sp_cdc_help_change_data_capture";
     private static final String GET_MAX_LSN = "SELECT sys.fn_cdc_get_max_lsn()";
     private static final String GET_MIN_LSN = "SELECT sys.fn_cdc_get_min_lsn('#')";
     private static final String GET_INCREMENT_LSN = "SELECT sys.fn_cdc_increment_lsn(?)";
+    private static final String GET_ALL_CHANGES_FOR_TABLE = "SELECT * FROM cdc.[fn_cdc_get_all_changes_#](?, ?, N'all update old') order by [__$start_lsn] ASC, [__$seqval] ASC, [__$operation] ASC";
 
     private static final String LSN_POSITION = "position";
+    private static final long DEFAULT_POLL_INTERVAL_MILLIS = 360;
+    private static final int OFFSET_COLUMNS = 4;
     private final Lock connectLock = new ReentrantLock();
     private volatile boolean connected;
-    private static String getAllChangesForTable;
     private static Set<String> tables;
     private static Set<SqlServerChangeTable> changeTables;
     private Connection connection;
@@ -76,7 +76,6 @@ public class SqlServerExtractor extends AbstractExtractor {
             enableDBCDC();
             enableTableCDC();
             changeTables = readChangeTables();
-            getAllChangesForTable = getAllChangesForTableSql();
             readLastLsn();
 
             worker = new Worker();
@@ -153,25 +152,11 @@ public class SqlServerExtractor extends AbstractExtractor {
         lastLsn = Lsn.valueOf(map.get(LSN_POSITION));
     }
 
-    private Lsn getIncrementLsn(Lsn lastLsn) {
-        return queryAndMap(GET_INCREMENT_LSN, statement -> statement.setBytes(1, lastLsn.getBinary()), rs -> Lsn.valueOf(rs.getBytes(1)));
-    }
-
-    private String getAllChangesForTableSql() {
-        // 2016以上版本支持UTC
-        String version = queryAndMap(GET_DATABASE_VERSION, rs -> rs.getString(1));
-        boolean supportsAtTimeZone = false;
-        if (version.startsWith("Microsoft SQL Server ")) {
-            supportsAtTimeZone = 2016 < Integer.valueOf(version.substring(21, 25));
-        }
-        return GET_ALL_CHANGES_FOR_TABLE.replaceFirst(STATEMENTS_PLACEHOLDER, Matcher.quoteReplacement(supportsAtTimeZone ? AT_TIME_ZONE_UTC : ""));
-    }
-
     private Set<String> readTables() {
         return queryAndMapList(GET_TABLE_LIST, rs -> {
             Set<String> table = new LinkedHashSet<>();
             while (rs.next()) {
-                if(filterTable.contains(rs.getString(1))){
+                if (filterTable.contains(rs.getString(1))) {
                     table.add(rs.getString(1));
                 }
             }
@@ -253,6 +238,51 @@ public class SqlServerExtractor extends AbstractExtractor {
         }
     }
 
+    private void pull(Lsn stopLsn) {
+        Lsn startLsn = queryAndMap(GET_INCREMENT_LSN, statement -> statement.setBytes(1, lastLsn.getBinary()), rs -> Lsn.valueOf(rs.getBytes(1)));
+        changeTables.forEach(changeTable -> {
+            final String query = GET_ALL_CHANGES_FOR_TABLE.replace(STATEMENTS_PLACEHOLDER, changeTable.getCaptureInstance());
+            List<List<Object>> list = queryAndMapList(query, statement -> {
+                statement.setBytes(1, startLsn.getBinary());
+                statement.setBytes(2, stopLsn.getBinary());
+            }, rs -> {
+                int columnCount = rs.getMetaData().getColumnCount();
+                List<List<Object>> data = new ArrayList<>(columnCount);
+                List<Object> row = null;
+                while (rs.next()) {
+                    row = new ArrayList<>(columnCount);
+                    for (int i = 1; i <= columnCount; i++) {
+                        row.add(rs.getObject(i));
+                    }
+                    data.add(row);
+                }
+                return data;
+            });
+
+            parseEvent(changeTable, list);
+        });
+    }
+
+    private void parseEvent(SqlServerChangeTable changeTable, List<List<Object>> list) {
+        if (CollectionUtils.isEmpty(list)) {
+            return;
+        }
+        list.forEach(row -> {
+            if (!CollectionUtils.isEmpty(row) && row.size() > OFFSET_COLUMNS) {
+                final int operation = (int) row.get(2);
+                List<Object> data = new ArrayList<>(row.subList(OFFSET_COLUMNS, row.size()));
+                if (TableOperation.isUpdate(operation)) {
+                    asynSendRowChangedEvent(new RowChangedEvent(changeTable.getTableName(), ConnectorConstant.OPERTION_UPDATE, Collections.EMPTY_LIST, data));
+                } else if (TableOperation.isInsert(operation)) {
+                    asynSendRowChangedEvent(new RowChangedEvent(changeTable.getTableName(), ConnectorConstant.OPERTION_INSERT, Collections.EMPTY_LIST, data));
+                } else if (TableOperation.isDelete(operation)) {
+                    asynSendRowChangedEvent(new RowChangedEvent(changeTable.getTableName(), ConnectorConstant.OPERTION_DELETE, data, Collections.EMPTY_LIST));
+                }
+
+            }
+        });
+    }
+
     private interface ResultSetMapper<T> {
         T apply(ResultSet rs) throws SQLException;
     }
@@ -292,7 +322,7 @@ public class SqlServerExtractor extends AbstractExtractor {
             rs = ps.executeQuery();
             apply = mapper.apply(rs);
         } catch (SQLServerException e) {
-            logger.warn(e.getMessage());
+            // 为过程或函数 cdc.fn_cdc_get_all_changes_ ...  提供的参数数目不足。
         } catch (Exception e) {
             logger.error(e.getMessage());
         } finally {
@@ -310,44 +340,28 @@ public class SqlServerExtractor extends AbstractExtractor {
                 try {
                     Lsn stopLsn = queryAndMap(GET_MAX_LSN, rs -> new Lsn(rs.getBytes(1)));
                     if (!stopLsn.isAvailable()) {
-                        TimeUnit.SECONDS.sleep(1);
+                        pause();
                         continue;
                     }
 
                     if (stopLsn.compareTo(lastLsn) <= 0) {
-                        TimeUnit.MICROSECONDS.sleep(200);
+                        pause();
                         continue;
                     }
 
-                    Lsn startLsn = getIncrementLsn(lastLsn);
-                    changeTables.forEach(changeTable -> {
-                        final String query = getAllChangesForTable.replace(STATEMENTS_PLACEHOLDER, changeTable.getCaptureInstance());
-                        queryAndMapList(query, statement -> {
-                            statement.setBytes(1, startLsn.getBinary());
-                            statement.setBytes(2, stopLsn.getBinary());
-                        }, rs -> {
-                            int columnCount = rs.getMetaData().getColumnCount();
-                            List<List<Object>> data = new ArrayList<>(columnCount);
-                            List<Object> row = null;
-                            while (rs.next()) {
-                                row = new ArrayList<>(columnCount);
-                                for (int i = 1; i <= columnCount; i++) {
-                                    row.add(rs.getObject(i));
-                                }
-                                // FIXME 解析增量数据
-                                logger.info(row.toString());
-                                data.add(row);
-                            }
-                            return data;
-                        });
-                    });
+                    pull(stopLsn);
 
                     lastLsn = stopLsn;
-                } catch (Exception e) {
+                } catch (InterruptedException e) {
                     break;
                 }
             }
         }
 
+        private void pause() throws InterruptedException {
+            TimeUnit.MICROSECONDS.sleep(DEFAULT_POLL_INTERVAL_MILLIS);
+        }
+
     }
+
 }
