08f6c075bbd2691fe4c584b271a027f0621c9671
AE86 <836391306@qq.com>
2021-08-06 17:52:59 +0800

    可选匹配相似表

--- dbsyncer-web/src/main/resources/static/js/mapping/add.js
+++ dbsyncer-web/src/main/resources/static/js/mapping/add.js
@@ -9,6 +9,15 @@ function submit(data) {
     });
 }
 
+// 绑定匹配相似表复选框事件
+function bindAutoMatchTableCheckBoxClick(){
+    $('#autoMatchTableSelect').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        labelHover: false,
+        cursor: true
+    });
+}
+
 $(function () {
     // 兼容IE PlaceHolder
     $('input[type="text"],input[type="password"],textarea').PlaceHolder();
@@ -18,6 +27,9 @@ $(function () {
         width: "100%",
         theme: "classic"
     });
+
+    // 绑定匹配相似表复选框事件
+    bindAutoMatchTableCheckBoxClick();
 
     //保存
     $("#mappingSubmitBtn").click(function () {
