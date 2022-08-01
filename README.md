b47808e9c2470083cfa6e54293e2a62f4414fda2
AE86 <836391306@qq.com>
2022-08-02 00:26:51 +0800

    fix

--- README.md
+++ README.md
@@ -176,10 +176,10 @@ $ ./build.sh
 ## 🐞常见问题
 * Mysql无法连接。默认使用的驱动版本为5.1.40，如果为mysql8需要手动替换驱动 [mysql-connector-java-8.0.21.jar](https://gitee.com/ghi/dbsyncer/attach_files) 
 * 有其他问题、建议或需求（如想支持某中间件，记得补充使用的版本号），欢迎大家提[【issuses】](https://gitee.com/ghi/dbsyncer/issues/new?issue%5Bassignee_id%5D=0&issue%5Bmilestone_id%5D=0)!
-* 在linux系统中，打包后，在bin文件夹下运行startup.sh前，要使用vim操作
+* 启动脚本编码格式有误，在linux系统中，打包后，在bin文件夹下运行startup.sh前，要使用vi操作
 ```bash
-vim startup.sh
-:set ff=unix 
+vi startup.sh
+:set ff=unix
 :wq
 ```
 再运行startup.sh
--- dbsyncer-listener/src/main/java/org/dbsyncer/listener/quartz/filter/YesDateFilter.java
+++ dbsyncer-listener/src/main/java/org/dbsyncer/listener/quartz/filter/YesDateFilter.java
@@ -3,9 +3,9 @@ package org.dbsyncer.listener.quartz.filter;
 import org.dbsyncer.common.util.DateFormatUtil;
 import org.dbsyncer.listener.quartz.QuartzFilter;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class YesDateFilter implements QuartzFilter {
 
@@ -17,19 +17,15 @@ public class YesDateFilter implements QuartzFilter {
 
     @Override
     public Object getObject() {
-        Date as = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
-        SimpleDateFormat matter1 = new SimpleDateFormat("yyyy-MM-dd");
-        String time = matter1.format(as) + " 00:00:00";
+        LocalDateTime localDateTime;
         if (!begin) {
-            time = matter1.format(as) + " 23:59:59";
+            // 2022-08-02 23:59:59
+            localDateTime = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59).withNano(999999999);
+        } else {
+            // 2022-08-02 00:00:00
+            localDateTime = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
         }
-        matter1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        try {
-            as = matter1.parse(time);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return as;
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
     @Override
@@ -46,4 +42,5 @@ public class YesDateFilter implements QuartzFilter {
     public boolean begin() {
         return begin;
     }
+
 }
