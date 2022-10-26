d9aca846f6c3038bf91bcdfccc40f5e627ce5dbd
王翔 <wangxiang@hdsxtech.com>
2022-10-26 10:34:16 +0800

    注释掉OracleSpatial支持--因为下载不到OracleSdo.jar

--- dbsyncer-connector/src/main/java/org/dbsyncer/connector/database/DatabaseValueMapper.java
+++ dbsyncer-connector/src/main/java/org/dbsyncer/connector/database/DatabaseValueMapper.java
@@ -2,7 +2,7 @@ package org.dbsyncer.connector.database;
 
 import com.microsoft.sqlserver.jdbc.Geometry;
 import oracle.jdbc.OracleConnection;
-import oracle.spatial.geometry.JGeometry;
+//import oracle.spatial.geometry.JGeometry;
 import org.dbsyncer.connector.ConnectorException;
 import org.dbsyncer.connector.database.ds.SimpleConnection;
 
@@ -48,14 +48,15 @@ public class DatabaseValueMapper {
     }
 
     public Struct getStruct(byte[] val) throws SQLException {
-        if (connection.getConnection() instanceof OracleConnection) {
-            OracleConnection conn = connection.unwrap(OracleConnection.class);
-            Geometry geometry = Geometry.deserialize(val);
-            Double x = geometry.getX();
-            Double y = geometry.getY();
-            JGeometry jGeometry = new JGeometry(x, y, 0);
-            return JGeometry.store(jGeometry, conn);
-        }
-        throw new ConnectorException(String.format("%s can not get STRUCT [%s], val [%s]", getClass().getSimpleName(), val.getClass(), val));
+        return null;
+//        if (connection.getConnection() instanceof OracleConnection) {
+//            OracleConnection conn = connection.unwrap(OracleConnection.class);
+//            Geometry geometry = Geometry.deserialize(val);
+//            Double x = geometry.getX();
+//            Double y = geometry.getY();
+//            JGeometry jGeometry = new JGeometry(x, y, 0);
+//            return JGeometry.store(jGeometry, conn);
+//        }
+//        throw new ConnectorException(String.format("%s can not get STRUCT [%s], val [%s]", getClass().getSimpleName(), val.getClass(), val));
     }
 }
