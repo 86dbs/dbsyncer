4f2fcdf588b2c3a0e3b1fd424eef62df748d0473
AE86 <836391306@qq.com>
2023-04-14 09:01:11 +0800

    fix shell

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mvn clean -f pom.xml
 mvn compile package -f pom.xml -Dmaven.test.skip=true
 
 CURRENT_DIR=$(pwd);
-mv -rvf $CURRENT_DIR/dbsyncer-web/target/dbsyncer-*.zip $CURRENT_DIR
+mv $CURRENT_DIR/dbsyncer-web/target/dbsyncer-*.zip $CURRENT_DIR
