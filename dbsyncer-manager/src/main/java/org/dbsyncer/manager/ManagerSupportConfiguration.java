361c6b62d158ab34577ddf27c1903026c044458c
AE86 <836391306@qq.com>
2023-11-19 23:35:18 +0800

    add deployment

---
+++ dbsyncer-manager/src/main/java/org/dbsyncer/manager/ManagerSupportConfiguration.java
@@ -0,0 +1,23 @@
+package org.dbsyncer.manager;
+
+import org.dbsyncer.manager.deployment.StandaloneProvider;
+import org.dbsyncer.sdk.spi.DeploymentService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author AE86
+ * @version 1.0.0
+ * @Date 2023-11-19 23:29
+ */
+@Configuration
+public class ManagerSupportConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DeploymentService deploymentService() {
+        return new StandaloneProvider();
+    }
+
+}
---
+++ dbsyncer-manager/src/main/java/org/dbsyncer/manager/deployment/StandaloneProvider.java
@@ -0,0 +1,16 @@
+/**
+ * DBSyncer Copyright 2020-2023 All Rights Reserved.
+ */
+package org.dbsyncer.manager.deployment;
+
+import org.dbsyncer.sdk.spi.DeploymentService;
+
+/**
+ * 单机部署
+ *
+ * @Version 1.0.0
+ * @Author AE86
+ * @Date 2023-11-19 23:29
+ */
+public class StandaloneProvider implements DeploymentService {
+}
