96db141201de8ce698a1be52578c0abdf6431d57
AE86 <836391306@qq.com>
2023-12-13 06:19:30 +0000

    支持BigDecimal转varchar

--- dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/connector/schema/LongVarcharValueMapper.java
+++ dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/connector/schema/LongVarcharValueMapper.java
@@ -1,9 +1,13 @@
+/**
+ * DBSyncer Copyright 2020-2023 All Rights Reserved.
+ */
 package org.dbsyncer.sdk.connector.schema;
 
 import org.dbsyncer.sdk.SdkException;
 import org.dbsyncer.sdk.connector.AbstractValueMapper;
 import org.dbsyncer.sdk.connector.ConnectorInstance;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 
 /**
@@ -23,6 +27,10 @@ public class LongVarcharValueMapper extends AbstractValueMapper<String> {
         }
         if (val instanceof Integer) {
             return String.valueOf(val);
+        }
+        if (val instanceof BigDecimal) {
+            BigDecimal bigDecimal = (BigDecimal) val;
+            return bigDecimal.toString();
         }
         throw new SdkException(String.format("%s can not find type [%s], val [%s]", getClass().getSimpleName(), val.getClass(), val));
     }
