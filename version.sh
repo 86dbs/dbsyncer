b3aeb34767df05d7407b0766353a9be5abb55f02
AE86 <836391306@qq.com>
2023-12-15 00:24:28 +0800

    add shell

---
+++ version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+VERSION=1.2.7_$(date +"%m%d")
+read -p "Please enter a new version number($VERSION)：" APP_VERSION
+if [ -z "$APP_VERSION" ]; then
+  APP_VERSION=$VERSION
+fi
+
+echo "Clean Project ..."
+mvn clean -f pom.xml
+
+echo "Update version ..."
+mvn versions:set -DnewVersion=$APP_VERSION -DprocessAllModules=true -DallowSnapshots=true -DgenerateBackupPoms=false
+mvn -N versions:update-child-modules
+mvn versions:commit
