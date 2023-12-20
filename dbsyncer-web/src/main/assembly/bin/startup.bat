48413e1fba3c5767380e399e8ad46838336d705b
AE86 <836391306@qq.com>
2023-12-21 01:44:24 +0800

    fix bat

--- dbsyncer-web/src/main/assembly/bin/startup.bat
+++ dbsyncer-web/src/main/assembly/bin/startup.bat
@@ -8,7 +8,6 @@ rem set CLASSPATH=.;%JAVA_HOME%\lib;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools
 for %%F in ("%~dp0\..\") do set "DBS_HOME=%%~dpF"
 echo DBS_HOME=%DBS_HOME%
 cd ../
-echo starting up ...
 
 set SERVER_OPTS=-Xms1024m -Xmx1024m -Xss1m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m
 rem debug model
@@ -17,15 +16,14 @@ rem set IPv4
 rem set SERVER_OPTS=%SERVER_OPTS% -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses
 
 rem set jmxremote args
-set HOST=127.0.0.1
-set JMXREMOTE_HOSTNAME=-Djava.rmi.server.hostname=%HOST%
+set JMXREMOTE_HOSTNAME=-Djava.rmi.server.hostname=127.0.0.1
 set JMXREMOTE_PORT=-Dcom.sun.management.jmxremote.port=15099
 set JMXREMOTE_SSL=-Dcom.sun.management.jmxremote.ssl=false
 set JMXREMOTE_AUTH=-Dcom.sun.management.jmxremote.authenticate=true
-set JMXREMOTE_ACCESS=-Dcom.sun.management.jmxremote.access.file=%DBS_HOME%\config\jmxremote.access
-set JMXREMOTE_PASSWORD=-Dcom.sun.management.jmxremote.password.file=%DBS_HOME%\config\jmxremote.password
+set JMXREMOTE_ACCESS=-Dcom.sun.management.jmxremote.access.file=%DBS_HOME%conf\jmxremote.access
+set JMXREMOTE_PASSWORD=-Dcom.sun.management.jmxremote.password.file=%DBS_HOME%conf\jmxremote.password
 rem jmxremote model
-rem SERVER_OPTS=%SERVER_OPTS% %JMXREMOTE_HOSTNAME% %JMXREMOTE_PORT% %JMXREMOTE_SSL% %JMXREMOTE_AUTH% %JMXREMOTE_ACCESS% %JMXREMOTE_PASSWORD%
+rem set SERVER_OPTS=%SERVER_OPTS% %JMXREMOTE_HOSTNAME% %JMXREMOTE_PORT% %JMXREMOTE_SSL% %JMXREMOTE_AUTH% %JMXREMOTE_ACCESS% %JMXREMOTE_PASSWORD%
 
 set ENCRYPT_FILE=%DBS_HOME%bin\dbsyncer_decrypt.dll
 if exist %ENCRYPT_FILE% (
