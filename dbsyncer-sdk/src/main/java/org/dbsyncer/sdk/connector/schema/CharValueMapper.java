6002cd8032a9287a13ec9a52ca5ff077919e938d
Peng <13397205+pengshengdong@user.noreply.gitee.com>
2024-04-19 11:43:55 +0800

    mysql 枚举类型增量转换异常

--- dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/connector/schema/CharValueMapper.java
+++ dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/connector/schema/CharValueMapper.java
@@ -3,6 +3,7 @@ package org.dbsyncer.sdk.connector.schema;
 import org.dbsyncer.sdk.SdkException;
 import org.dbsyncer.sdk.connector.AbstractValueMapper;
 import org.dbsyncer.sdk.connector.ConnectorInstance;
+import org.dbsyncer.sdk.connector.CustomType;
 
 /**
  * @author AE86
@@ -13,6 +14,11 @@ public class CharValueMapper extends AbstractValueMapper<String> {
 
     @Override
     protected String convert(ConnectorInstance connectorInstance, Object val) {
+        if (null != val) {
+            if (val instanceof  Integer) {
+                return  String.valueOf(val);
+            }
+        }
         throw new SdkException(String.format("%s can not find type [%s], val [%s]", getClass().getSimpleName(), val.getClass(), val));
     }
 }
