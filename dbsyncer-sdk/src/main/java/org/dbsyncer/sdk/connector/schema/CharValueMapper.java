6bbbef2e0d898a5805b693b46b45e91caaa935e8
AE86 <836391306@qq.com>
2024-04-19 07:30:37 +0000

    fmt

--- dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/connector/schema/CharValueMapper.java
+++ dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/connector/schema/CharValueMapper.java
@@ -3,7 +3,6 @@ package org.dbsyncer.sdk.connector.schema;
 import org.dbsyncer.sdk.SdkException;
 import org.dbsyncer.sdk.connector.AbstractValueMapper;
 import org.dbsyncer.sdk.connector.ConnectorInstance;
-import org.dbsyncer.sdk.connector.CustomType;
 
 /**
  * @author AE86
@@ -15,8 +14,8 @@ public class CharValueMapper extends AbstractValueMapper<String> {
     @Override
     protected String convert(ConnectorInstance connectorInstance, Object val) {
         if (null != val) {
-            if (val instanceof  Integer) {
-                return  String.valueOf(val);
+            if (val instanceof Integer) {
+                return String.valueOf(val);
             }
         }
         throw new SdkException(String.format("%s can not find type [%s], val [%s]", getClass().getSimpleName(), val.getClass(), val));
