aebb6be1ac56acc2c1c7e36f813c8f7d2e28a7c7
AE86 <836391306@qq.com>
2024-05-09 09:59:44 +0000

    fix bug

--- dbsyncer-parser/src/main/java/org/dbsyncer/parser/convert/handler/StringToFormatDateHandler.java
+++ dbsyncer-parser/src/main/java/org/dbsyncer/parser/convert/handler/StringToFormatDateHandler.java
@@ -1,11 +1,13 @@
+/**
+ * DBSyncer Copyright 2020-2024 All Rights Reserved.
+ */
 package org.dbsyncer.parser.convert.handler;
 
 import org.dbsyncer.common.util.DateFormatUtil;
 import org.dbsyncer.parser.convert.AbstractHandler;
 
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 字符串转日期自定义格式
@@ -17,11 +19,9 @@ public class StringToFormatDateHandler extends AbstractHandler {
     @Override
     public Object convert(String args, Object value) throws ParseException {
 
-        DateFormat dateFormat = new SimpleDateFormat(args);
-
         if (value instanceof String) {
             String s = (String) value;
-            return DateFormatUtil.stringToTimestamp(s, dateFormat);
+            return DateFormatUtil.stringToDate(s, DateTimeFormatter.ofPattern(args));
         }
         return value;
     }
