f888f77e155da0f549c5f03678f5685dad2007cb
AE86 <836391306@qq.com>
2024-06-01 00:34:40 +0800

    add spi

--- dbsyncer-manager/src/main/java/org/dbsyncer/manager/ManagerSupportConfiguration.java
+++ dbsyncer-manager/src/main/java/org/dbsyncer/manager/ManagerSupportConfiguration.java
@@ -1,3 +1,6 @@
+/**
+ * DBSyncer Copyright 2020-2024 All Rights Reserved.
+ */
 package org.dbsyncer.manager;
 
 import org.dbsyncer.manager.deployment.StandaloneProvider;
@@ -5,6 +8,8 @@ import org.dbsyncer.sdk.spi.DeploymentService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ServiceLoader;
 
 /**
  * @author AE86
@@ -17,6 +22,10 @@ public class ManagerSupportConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public DeploymentService deploymentService() {
+        ServiceLoader<DeploymentService> services = ServiceLoader.load(DeploymentService.class, Thread.currentThread().getContextClassLoader());
+        for (DeploymentService s : services) {
+            return s;
+        }
         return new StandaloneProvider();
     }
 
