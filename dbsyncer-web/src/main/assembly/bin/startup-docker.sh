1b14ad1f5fba587162d8da2eca98c8a068a8bda3
穿云 <chuanyun@scxhtb.com>
2024-08-03 21:32:01 +0800

    fix bug

--- dbsyncer-web/src/main/assembly/bin/startup-docker.sh
+++ dbsyncer-web/src/main/assembly/bin/startup-docker.sh
@@ -51,8 +51,8 @@ if [ -e "$DBS_HOME/bin/$ENCRYPT_FILE" ]; then
   SERVER_OPTS="$SERVER_OPTS -agentpath:$DBS_HOME/bin/$ENCRYPT_FILE"
 fi
 SERVER_OPTS="$SERVER_OPTS -Djava.ext.dirs=$JAVA_HOME/jre/lib/ext:$DBS_HOME/lib \
--Dspring.config.location=$CONFIG_PATH" \
--Dfile.encoding=UTF-8 -Duser.dir=$DBS_HOME
+-Dspring.config.location=$CONFIG_PATH \
+-Dfile.encoding=UTF-8 -Duser.dir=$DBS_HOME"
 
 # execute command
 echo $SERVER_OPTS
