efc32d8d94824926085a725b7936302b39e20dfa
穿云 <chuanyun@scxhtb.com>
2024-08-03 21:37:00 +0800

    fix bug

--- dbsyncer-web/src/main/assembly/bin/startup.sh
+++ dbsyncer-web/src/main/assembly/bin/startup.sh
@@ -50,12 +50,13 @@ fi
 if [ -e "$DBS_HOME/bin/$ENCRYPT_FILE" ]; then
   SERVER_OPTS="$SERVER_OPTS -agentpath:$DBS_HOME/bin/$ENCRYPT_FILE"
 fi
-SERVER_OPTS="$SERVER_OPTS -Djava.ext.dirs=$JAVA_HOME/jre/lib/ext:$DBS_HOME/lib \
+SERVER_OPTS="$SERVER_OPTS \
+-Djava.ext.dirs=$JAVA_HOME/jre/lib/ext:$DBS_HOME/lib \
 -Dspring.config.location=$CONFIG_PATH \
 -Dfile.encoding=UTF-8 -Duser.dir=$DBS_HOME"
 
 # execute command
 echo $SERVER_OPTS
-java $SERVER_OPTS  $APP > /dev/null & echo $! > $DBS_HOME/tmp.pid
+java $SERVER_OPTS $APP > /dev/null & echo $! > $DBS_HOME/tmp.pid
 echo 'Start successfully!'
 exit 1
