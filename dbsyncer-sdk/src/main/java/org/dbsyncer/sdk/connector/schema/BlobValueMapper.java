55c63be2ce86389ba2a678ebcb32b9e19740b5ab
pengshengdong <5201314psd./>
2024-12-12 23:36:56 +0800

    增加oracle  Blob类型转换

--- dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/connector/schema/BlobValueMapper.java
+++ dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/connector/schema/BlobValueMapper.java
@@ -1,20 +1,16 @@
 package org.dbsyncer.sdk.connector.schema;
 
+import org.dbsyncer.common.util.StringUtil;
 import org.dbsyncer.sdk.SdkException;
 import org.dbsyncer.sdk.connector.AbstractValueMapper;
 import org.dbsyncer.sdk.connector.ConnectorInstance;
-
-import javax.sql.rowset.serial.SerialBlob;
-import java.nio.charset.StandardCharsets;
-import java.sql.Blob;
-import java.sql.SQLException;
 
 /**
  * @author AE86
  * @version 1.0.0
  * @date 2022/8/25 0:07
  */
-public class BlobValueMapper extends AbstractValueMapper<Blob> {
+public class BlobValueMapper extends AbstractValueMapper<byte[]> {
 
     @Override
     protected boolean skipConvert(Object val) {
@@ -22,15 +18,14 @@ public class BlobValueMapper extends AbstractValueMapper<Blob> {
     }
 
     @Override
-    protected Blob convert(ConnectorInstance connectorInstance, Object val) {
+    protected byte[] convert(ConnectorInstance connectorInstance, Object val) {
         if (val instanceof String) {
             String s = (String) val;
-            try {
-                return new SerialBlob(s.getBytes(StandardCharsets.UTF_8));
-            } catch (SQLException e) {
-                throw new SdkException(String.format("%s can not find type [%s], val [%s]", getClass().getSimpleName(), val.getClass(), val));
+            if (s.startsWith("HEXTORAW(")) {
+                return StringUtil.hexStringToByteArray(s.replace("HEXTORAW('", "").replace("')", ""));
             }
         }
         throw new SdkException(String.format("%s can not find type [%s], val [%s]", getClass().getSimpleName(), val.getClass(), val));
     }
+
 }
