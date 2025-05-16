758af79e07a1e8ff3d7db29de16a7e490d85e181
穿云 <chuanyun@scxhtb.com>
2025-05-17 00:43:59 +0800

    add spi

---
+++ dbsyncer-sdk/src/main/java/org/dbsyncer/sdk/spi/CompareDataService.java
@@ -0,0 +1,14 @@
+/**
+ * DBSyncer Copyright 2020-2025 All Rights Reserved.
+ */
+package org.dbsyncer.sdk.spi;
+
+/**
+ * 数据校验服务
+ *
+ * @Author 穿云
+ * @Version 1.0.0
+ * @Date 2025-05-12 23:36
+ */
+public interface CompareDataService {
+}
