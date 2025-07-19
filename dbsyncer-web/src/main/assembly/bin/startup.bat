656820e2b092fb291c9c1fcc9bce51ca5a9113d7
穿云 <chuanyun@scxhtb.com>
2025-07-19 23:03:42 +0800

    简化脚本参数

--- dbsyncer-web/src/main/assembly/bin/startup.bat
+++ dbsyncer-web/src/main/assembly/bin/startup.bat
@@ -2,28 +2,14 @@
 
 rem set up environment for Java
 rem set JAVA_HOME=D:\java\jdk1.8.0_202
-rem set PATH=%JAVA_HOME%\bin;%JAVA_HOME%\jre\bin;
-rem set CLASSPATH=.;%JAVA_HOME%\lib;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar
 
 for %%F in ("%~dp0\..\") do set "DBS_HOME=%%~dpF"
 echo DBS_HOME=%DBS_HOME%
 cd ../
 
 set SERVER_OPTS=-Xms3800m -Xmx3800m -Xmn1500m -Xss512k -XX:MetaspaceSize=192m -XX:+DisableAttachMechanism
-rem debug model
-rem set SERVER_OPTS=%SERVER_OPTS% -Djava.compiler=NONE -Xnoagent -Xdebug -Xrunjdwp:transport=dt_socket,address=15005,server=y,suspend=n
 rem set IPv4
 rem set SERVER_OPTS=%SERVER_OPTS% -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses
-
-rem set jmxremote args
-set JMXREMOTE_HOSTNAME=-Djava.rmi.server.hostname=127.0.0.1
-set JMXREMOTE_PORT=-Dcom.sun.management.jmxremote.port=15099
-set JMXREMOTE_SSL=-Dcom.sun.management.jmxremote.ssl=false
-set JMXREMOTE_AUTH=-Dcom.sun.management.jmxremote.authenticate=true
-set JMXREMOTE_ACCESS=-Dcom.sun.management.jmxremote.access.file=%DBS_HOME%conf\jmxremote.access
-set JMXREMOTE_PASSWORD=-Dcom.sun.management.jmxremote.password.file=%DBS_HOME%conf\jmxremote.password
-rem jmxremote model
-rem set SERVER_OPTS=%SERVER_OPTS% %JMXREMOTE_HOSTNAME% %JMXREMOTE_PORT% %JMXREMOTE_SSL% %JMXREMOTE_AUTH% %JMXREMOTE_ACCESS% %JMXREMOTE_PASSWORD%
 
 set ENCRYPT_FILE=%DBS_HOME%bin\libDBSyncer.dll
 if exist %ENCRYPT_FILE% (
