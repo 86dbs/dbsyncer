83ac8ccc2a1d1aef9d1628de0ad484b5e8087d71
穿云 <chuanyun@scxhtb.com>
2025-10-22 01:33:33 +0800

    支持pg bytea https://gitee.com/ghi/dbsyncer/issues/ID202U

---
+++ dbsyncer-connector/dbsyncer-connector-postgresql/src/main/java/org/dbsyncer/connector/postgresql/schema/support/PostgreSQLBytesType.java
@@ -0,0 +1,44 @@
+/**
+ * DBSyncer Copyright 2020-2025 All Rights Reserved.
+ */
+package org.dbsyncer.connector.postgresql.schema.support;
+
+import org.dbsyncer.sdk.model.Field;
+import org.dbsyncer.sdk.schema.support.BytesType;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @Author 穿云
+ * @Version 1.0.0
+ * @Date 2025-10-22 01:27
+ */
+public class PostgreSQLBytesType extends BytesType {
+
+    private enum TypeEnum {
+        BYTEA("bytea");
+
+        private final String value;
+
+        TypeEnum(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    @Override
+    public Set<String> getSupportedTypeName() {
+        return Arrays.stream(PostgreSQLBytesType.TypeEnum.values()).map(PostgreSQLBytesType.TypeEnum::getValue).collect(Collectors.toSet());
+    }
+
+    @Override
+    protected byte[] merge(Object val, Field field) {
+        return throwUnsupportedException(val, field);
+    }
+
+}
