93b039ee76be0ae0dfd9adbe19e970697ff62690
穿云 <chuanyun@scxhtb.com>
2025-10-27 00:03:48 +0800

    update ui

--- dbsyncer-web/src/main/resources/static/js/index.js
+++ dbsyncer-web/src/main/resources/static/js/index.js
@@ -1,86 +1,75 @@
 // ******************* 初始化 *****************************
 // 默认绑定菜单事件
-$(function () {
-    // 刷新登录用户
-    refreshLoginUser();
-    // 刷新授权信息
-    refreshLicenseInfo();
+// 获取项目地址
+const $location = (window.location + '').split('/');
+const $path = document.location.pathname;
+const $basePath = $location[0] + '//' + $location[2] + $path.substr(0, $path.substr(1).indexOf("/") + 1);
 
-    // 初始化版权信息
-    doGetter("/index/version.json", {}, function (data) {
-        if (data.success == true) {
-            // 获取底部版权信息
-            $("#appCopyRight").html(data.resultValue.appCopyRight);
-            settings.watermark_txt = data.resultValue.watermark;
-            watermark();
-        }
+// 全局加载页面
+function doLoader(url) {
+    // 使用统一的内容区域
+    const contentToShow = $('#mainContent');
+    if (contentToShow.length) {
+        // 显示加载状态
+        $.loadingT(true);
+
+        // 加载页面内容
+        contentToShow.load($basePath + url, function (response, status, xhr) {
+            $.loadingT(false);
+            if (status !== 'success') {
+                alert('页面加载失败，请稍后重试');
+            }
+        });
+    }
+}
+
+function doPoster(url, params, action) {
+    $.loadingT(true);
+    $.post($basePath + url, params, function (data) {
+        $.loadingT(false);
+        action(data);
+    }).error(function (xhr, status, info) {
+        $.loadingT(false);
+        alert("访问异常，请刷新或重试.");
     });
+}
+
+$(function () {
+    // // 刷新登录用户
+    // refreshLoginUser();
+    // // 刷新授权信息
+    // refreshLicenseInfo();
+
+    // // 初始化版权信息
+    // doGetter("/index/version.json", {}, function (data) {
+    //     if (data.success == true) {
+    //         // 获取底部版权信息
+    //         $("#appCopyRight").html(data.resultValue.appCopyRight);
+    //         settings.watermark_txt = data.resultValue.watermark;
+    //         watermark();
+    //     }
+    // });
 
     // 修改登录用户
     $("#edit_personal").click(function () {
         doLoader("/user/page/edit?username=" + $(this).attr("username"));
     });
-
+    //
     // 注销
     $("#nav_logout").click(function () {
-        // 确认框确认是否注销
-        BootstrapDialog.show({
-            title: "提示",
-            type: BootstrapDialog.TYPE_INFO,
-            message: "确认注销帐号？",
-            size: BootstrapDialog.SIZE_NORMAL,
-            buttons: [{
-                label: "确定",
-                action: function (dialog) {
-                    doPoster("/logout", null, function (data) {
-                        location.href = $basePath;
-                    });
-                    dialog.close();
-                }
-            }, {
-                label: "取消",
-                action: function (dialog) {
-                    dialog.close();
-                }
-            }]
+        doPoster("/logout", null, function (data) {
+            location.href = $basePath;
         });
     });
 
-    // 绑定所有的菜单链接点击事件，根据不同的URL加载页面
-    $("#menu li a[url]").click(function () {
-        var route = $(this).data('route');
-        // 加载页面
-        doLoader($(this).attr("url"), route);
-        // 加载页面
-        const contents = document.querySelectorAll('.contentDiv');
-        contents.forEach(function (content) {
-            content.classList.add('hidden');
-        });
-        if (route === 1) {
-            if (timer != null) {
-                clearInterval(timer);
-                timer = null;
-            }
-        } else if (route === 2) {
-            if (timer2 != null) {
-                clearInterval(timer2);
-                timer2 = null;
-            }
-        }
-        const contentToShow = $('#initContainer' + route);
-        if (contentToShow) {
-            contentToShow.removeClass('hidden');
-        }
-
-    });
-
-    // 头部导航栏选中切换事件
-    var $menu = $('#menu > li');
-    $menu.click(function () {
-        $menu.removeClass('active');
+    // 新导航链接点击事件
+    $('.sidebar-item[url]').on('click', function(e) {
+        e.preventDefault();
+        const url = $(this).attr('url');
+        // 更新活动状态
+        $('.sidebar-item').removeClass('active');
         $(this).addClass('active');
+        // 加载页面
+        doLoader(url);
     });
-
-    // 显示主页
-    backIndexPage();
 });
