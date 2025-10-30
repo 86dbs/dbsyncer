f28567400d03e6a4d15da674173cd236b4ee39e4
穿云 <chuanyun@scxhtb.com>
2025-10-30 23:49:28 +0800

    重构用户管理

--- dbsyncer-web/src/main/resources/static/js/user/index.js
+++ dbsyncer-web/src/main/resources/static/js/user/index.js
@@ -1,43 +1,41 @@
 $(function () {
     // 绑定添加用户按钮事件
-    $("#addUserBtn").click(function () {
+    $("#addUserBtn").on('click', function () {
         doLoader("/user/page/add");
     });
 
     // 绑定修改用户按钮事件
-    $(".editUserBtn").click(function () {
-        doLoader("/user/page/edit?username=" + $(this).attr("id"));
+    $(".editUserBtn").on('click', function () {
+        const username = $(this).data('username');
+        doLoader("/user/page/edit?username=" + username);
     });
 
     // 绑定删除用户按钮事件
-    $(".removeUserBtn").click(function () {
-        const $username = $(this).attr("id");
-        // 确认框确认是否删除用户
-        BootstrapDialog.show({
-            title: "提示",
-            type: BootstrapDialog.TYPE_INFO,
-            message: "确认删除帐号？",
-            size: BootstrapDialog.SIZE_NORMAL,
-            buttons: [{
-                label: "确定",
-                action: function (dialog) {
-                    doPoster('/user/remove', {username: $username}, function (data) {
-                        if (data.success == true) {
-                            bootGrowl("删除用户成功！", "success");
-                        } else {
-                            bootGrowl(data.resultValue, "danger");
-                        }
+    $(".removeUserBtn").on('click', function () {
+        const username = $(this).data('username');
+        const $btn = $(this);
+        
+        // 原生确认对话框
+        if (confirm('确认删除用户 "' + username + '" 吗？此操作不可恢复。')) {
+            // 禁用按钮，防止重复点击
+            $btn.prop('disabled', true);
+            const originalText = $btn.html();
+            $btn.html('<i class="fa fa-spinner fa-spin"></i> 删除中...');
+            
+            doPoster('/user/remove', { username: username }, function (data) {
+                $btn.prop('disabled', false);
+                $btn.html(originalText);
+                
+                if (data.success == true) {
+                    bootGrowl("删除用户成功！", "success");
+                    // 延迟刷新，让用户看到成功提示
+                    setTimeout(function() {
                         doLoader("/user?refresh=" + new Date().getTime());
-                    });
-                    dialog.close();
+                    }, 500);
+                } else {
+                    bootGrowl(data.resultValue || "删除失败", "danger");
                 }
-            }, {
-                label: "取消",
-                action: function (dialog) {
-                    dialog.close();
-                }
-            }]
-        });
+            });
+        }
     });
-
 })
