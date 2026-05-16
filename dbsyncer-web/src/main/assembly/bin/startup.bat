97f100c4b98d4822a73cf10c2da51f6ffde64e11
AE86 <836391306@qq.com>
2026-05-16 06:12:48 +0000

    update dbsyncer-web/src/main/assembly/bin/startup.bat.

--- dbsyncer-web/src/main/assembly/bin/startup.bat
+++ dbsyncer-web/src/main/assembly/bin/startup.bat
@@ -7,11 +7,6 @@ for %%F in ("%~dp0\..\") do set "DBS_HOME=%%~dpF"
 echo DBS_HOME=%DBS_HOME%
 cd ../
 
-if "%JAVA_HOME%"=="" (
-  echo ERROR: JAVA_HOME is not set. Please set JAVA_HOME to your JDK 8 installation.
-  exit /b 1
-)
-
 set "JAVA_EXT_DIR=%JAVA_HOME%\jre\lib\ext"
 if not exist "%JAVA_EXT_DIR%" set "JAVA_EXT_DIR=%JAVA_HOME%\lib\ext"
 if not exist "%JAVA_EXT_DIR%" (
@@ -34,9 +29,7 @@ set SERVER_OPTS=%SERVER_OPTS% -Dspring.config.location=%DBS_HOME%conf\applicatio
 set SERVER_OPTS=%SERVER_OPTS% -DLOG_PATH=%DBS_HOME%\logs
 set SERVER_OPTS=%SERVER_OPTS% -Dsun.stdout.encoding=UTF-8 -Dfile.encoding=UTF-8 -Duser.dir=%DBS_HOME%
 set SERVER_OPTS=%SERVER_OPTS% -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:ParallelGCThreads=4 -XX:+CMSClassUnloadingEnabled -XX:+DisableExplicitGC
-rem GC 日志已关闭，需要时可取消下一行
-set SERVER_OPTS=%SERVER_OPTS% -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=68
-rem set SERVER_OPTS=%SERVER_OPTS% -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps
+set SERVER_OPTS=%SERVER_OPTS% -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=68 -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps
 set SERVER_OPTS=%SERVER_OPTS% -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=%DBS_HOME%logs -XX:ErrorFile=%DBS_HOME%logs\hs_err.log
 
 echo %SERVER_OPTS%
