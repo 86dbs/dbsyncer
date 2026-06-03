0f5ccbe92ce805c3eabc6c6b8c40a9692d2e238a
穿云 <chuanyun@scxhtb.com>
2026-06-04 00:16:26 +0800

    新增OceanBase连接器

---
+++ dbsyncer-connector/dbsyncer-connector-oceanbase/src/main/java/org/dbsyncer/connector/oceanbase/OceanBaseConnector.java
@@ -0,0 +1,7 @@
+package org.dbsyncer.connector.oceanbase;
+
+public final class OceanBaseConnector {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
