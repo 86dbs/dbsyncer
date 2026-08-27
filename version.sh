5409267ede25dc0e9aa9500c9ec6948e4df763c3
穿云 <chuanyun@scxhtb.com>
2026-08-28 00:28:42 +0800

    升级版本

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.1.2_$(date +"%m%d")
+VERSION=2.1.5_$(date +"%m%d")
 read -p "Please enter a new version number($VERSION)：" APP_VERSION
 if [ -z "$APP_VERSION" ]; then
   APP_VERSION=$VERSION
